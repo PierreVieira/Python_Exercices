2c69fc10cf077d1067439f197dcdd706362681bc
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2019-12-18 20:37:32 -0300

    Rename 5.py to 05.py

---
+++ Seção 4 - Variáveis e Tipos de Dados/05.py
@@ -0,0 +1,2 @@
+number = float(input('Informe um número real: '))
+print('A quinta parte de {} é {:.2f}'.format(number, number/5))
--- Seção 4 - Variáveis e Tipos de Dados/5.py
+++
@@ -1,2 +0,0 @@
-number = float(input('Informe um número real: '))
-print('A quinta parte de {} é {:.2f}'.format(number, number/5))
